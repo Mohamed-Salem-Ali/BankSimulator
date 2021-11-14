b190ce5d5e62fec7d232e9b2622dfbdad87479d2
Mohamed-Salem-Ali <mohamedsalem0100411@sci.asu.edu.eg>
2021-11-14 22:37:02 +0200

    update

--- generateUsers.py
+++ generateUsers.py
@@ -1,49 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import json 
 import time
 from faker import Faker
 import random
 from random import randint
+from threading import Timer
+
+
+# In[2]:
+
+
 fake = Faker('en_US')
 user_data=[]
 
-def createRandomData(dataNumber):
-    for i in range(dataNumber):
-        user_data.append(
-        {'id':i+1
-        ,'name': fake.name()
-        ,'age': random.randrange(18,100)
-        ,'address':fake.address()})
 
-def write_json(new_data, filename='users1.json'):
+# In[3]:
+
+
+def write_json(numberOfData, filename):
+    
     with open(filename,'r+') as file:
-        
+      
         # First we load existing data into a dict.
         file_data = json.load(file)
         
         # print("file1= ",file_data,len(file_data))
-        
+        index=file_data[-1]['id']
         # Join new_data with file_data inside emp_details
-        for i in range(len(new_data)):
-            file_data.append(new_data[i])
+        for i in range(1, numberOfData+1):
+            file_data.append( 
+            {'id':index+i
+            ,'name': fake.name()
+            ,'age': random.randrange(18,100)
+            ,'address':fake.address()})
         
         # Sets file's current position at offset.
         file.seek(0)
         
         # convert back to json.
         json.dump(file_data, file, indent = 4)
+        #Timer(5.0,write_json(numberOfData, filename)).start()
+        print(f"\n Added {numberOfData} user to the JSON file")
+        print(f"\n Total number of users is {file_data[-1]['id']}")
  
-createRandomData(1000)
 
 
-# jsonFile = open("users1.json", "r")
-# data = json.load(jsonFile)
-# print(data,len(user_data))
-write_json(user_data)
-# file = "users.json"
-# def write_json(data,fileName="users.json"):
-#     with open(fileName,'w') as f:
-#         json.dump(data,f,indent=4)
-#write_json(user_data[2])
-#print(user_data[2])
-#for i in range(len(user_data)): 
-#   print(user_data[i],"\n")
+# In[1]:
+
+
+def main():
+    print('-'*40)
+    print('\n Generate JSON data for users ')
+    print('\n Mohamed Salem Ali \n')
+    print('-'*40)
+ 
+    while True:
+        try:
+            numberOfData = int(input("\nEnter the number of users you want to generate : "))
+            print(f"\ngenerating {numberOfData} user")
+            write_json(numberOfData,'users1.json')
+        except ValueError:
+            print("\nNo.. input is not a number.")
+            continue
+
+        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        if restart.lower() != 'yes':
+            break
+
+
+if __name__ == "__main__":
+	main()
+
+
+# In[ ]:
+
+
+
+
